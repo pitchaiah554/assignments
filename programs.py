ba94420dd15ac3af0e771d78ecdebaa8c3c0a284
mohanpemmadi <connectpypi@gmail.com>
2022-06-14 14:57:52 +0530

    added factorial file

--- programs.py
+++ programs.py
@@ -1,12 +1,4 @@
-def factorial(num):
-    fact = 1
-    if num == 0:
-        print("The factorial of 0 is 1")
-    else:
-        for i in range(1,num + 1):
-            fact  = fact*i
-        print("The factorial of", num,"is",fact)
-# factorial(5)
+
 
 def remove_duplicates(st):
     # result = '#'.join(['a','b','a'])
